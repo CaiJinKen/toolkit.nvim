bd9db7cad3d9cff42573a259ae98d11c467fde07
CaiJinKen <linuxbar@163.com>
2024-09-03 14:36:45 +0800

    add trace log

--- lua/toolkit/log.lua
+++ lua/toolkit/log.lua
@@ -2,35 +2,39 @@ local M = {}
 
 -- convert param to table
 function M.ensure_table(param)
-	if type(param) ~= "table" then
-		return { param }
-	end
-	return param
+  if type(param) ~= "table" then
+    return { param }
+  end
+  return param
 end
 
 function M.log(msgs, log_level, opts)
-	local params = M.ensure_table(msgs)
-	if #params == 0 then
-		return
-	end
+  local params = M.ensure_table(msgs)
+  if #params == 0 then
+    return
+  end
 
-	vim.notify(table.concat(params, " "), log_level, opts)
+  vim.notify(table.concat(params, " "), log_level, opts)
 end
 
 function M.error(msgs)
-	M.log(msgs, vim.log.levels.ERROR, {})
+  M.log(msgs, vim.log.levels.ERROR, {})
 end
 
 function M.warn(msgs)
-	M.log(msgs, vim.log.levels.WARN, {})
+  M.log(msgs, vim.log.levels.WARN, {})
 end
 
 function M.info(msgs)
-	M.log(msgs, vim.log.levels.INFO, {})
+  M.log(msgs, vim.log.levels.INFO, {})
 end
 
 function M.debug(msgs)
-	m.log(msgs, vim.log.levels.DEBUG, {})
+  m.log(msgs, vim.log.levels.DEBUG, {})
+end
+
+function M.trace(msgs)
+  m.log(msgs, vim.log.levels.TRACE)
 end
 
 return M
