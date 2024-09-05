c43473545d656882e8adaa68881ac9e140dfdea0
CaiJinKen <linuxbar@163.com>
2024-09-05 15:00:43 +0800

    update git repo check

--- lua/toolkit/misc.lua
+++ lua/toolkit/misc.lua
@@ -2,24 +2,29 @@ local M = {}
 
 -- return true when current is git project
 function M.is_git_repo()
-	local handle = io.popen("git rev-parse --is-inside-work-tree 2>/dev/null")
-	local result = handle:read("*a")
-	handle:close()
-	return result:match("true") ~= nil
+  local result = vim.fn.systemlist('git rev-parse --is-inside-work-tree')
+  if vim.v.shell_error ~= 0 then
+    return false
+  end
+  if result[1] == 'true' then
+    return true
+  else
+    return false
+  end
 end
 
 -- get selection positions
 function M.get_visual_selection()
-	local start_pos = vim.fn.getpos("'<")
-	local end_pos = vim.fn.getpos("'>")
+  local start_pos = vim.fn.getpos("'<")
+  local end_pos = vim.fn.getpos("'>")
 
-	local start_line = start_pos[2]
-	local start_col = start_pos[3]
+  local start_line = start_pos[2]
+  local start_col = start_pos[3]
 
-	local end_line = end_pos[2]
-	local end_col = end_pos[3]
+  local end_line = end_pos[2]
+  local end_col = end_pos[3]
 
-	return start_line, start_col, end_line, end_col
+  return start_line, start_col, end_line, end_col
 end
 
 return M
